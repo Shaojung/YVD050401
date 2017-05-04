1a6f3265c27657e4d1ce84cb92297db1104d8813
Shaojung Lu <shaojung@gmail.com>
2017-05-04 10:11:22 +0800

    add Service

--- app/src/main/java/com/example/yvtc/yvd050401/MyService.java
+++ app/src/main/java/com/example/yvtc/yvd050401/MyService.java
@@ -2,13 +2,23 @@ package com.example.yvtc.yvd050401;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.util.Log;
 
 public class MyService extends Service {
+    Handler handler = new Handler();
     public MyService() {
     }
+
+    Runnable showTime = new Runnable() {
+        @Override
+        public void run() {
+            Log.d("SER1", "Time:" + new java.util.Date());
+            handler.postDelayed(this, 1000);
+        }
+    };
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,6 +27,7 @@ public class MyService extends Service {
         Log.d("SER1", "onBind");
         return null;
     }
+
 
     @Override
     public void onCreate() {
@@ -27,6 +38,7 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("SER1", "This is onStartCommand");
+        handler.post(showTime);
         return super.onStartCommand(intent, flags, startId);
     }
 }
